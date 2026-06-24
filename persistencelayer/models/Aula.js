dc4e8f508e43b5ddc79adf8fd2eaf39f3372c4bc
yCAFEEE <yuridan2705@gmail.com>
2026-06-24 13:24:01 -0300

    retirar required da url de aula

--- persistencelayer/models/Aula.js
+++ persistencelayer/models/Aula.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const AulaSchema = new mongoose.Schema({
     titulo: { type: String, required: true },
     desc: { type: String, required: true },
-    url: { type: String, required: true },
+    url: { type: String },
     explicacao: { type: String, required: true },
     exercicios: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exercicio" }]
 });
